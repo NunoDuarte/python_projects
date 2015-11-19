3020c0c008ee30c6ee2ba9fa116b8d91fcc28c51
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2015-11-19 15:25:11 +0000

    Check clauses might be correct; Add return for False or True statement; not quite working

--- DPLL.py
+++ DPLL.py
@@ -13,38 +13,50 @@ class DPLL(object):
         clauses = self.KB;
         symbols = list(string.ascii_uppercase[0:self.variables]);
         
-        return DPLL.search(self, clauses, symbols, {})
+        return DPLL.search(self, clauses, symbols, {}, [], [])
     
-    def search(self, clauses, symbols, model):
-        n = DPLL.check_clauses(self, clauses, model);
-        print (n)
-        print (model)
-        if n == 0:
+    def search(self, clauses, symbols, model, variable, value):
+        if variable: 
+            model[variable] = value;
+        if DPLL.check_false_clauses(self, clauses, model) == 0:
+            del model[variable]         
             return False
-        if n == self.clauses:
+        if DPLL.check_clauses(self, clauses, model) == self.clauses:    
             return True
+        [P, value] = DPLL.find_pure_symbol(self, symbols, clauses, model)
+        if P is not None:
+            number = DPLL.pop_symbol(self, symbols, P)
+            symbols.pop(number)
+            DPLL.search(self, clauses, symbols, model, P, value)
+        [P, value] = DPLL.find_unit_clause(self, symbols, clauses, model)
+        if P is not None:
+            number = DPLL.pop_symbol(self, symbols, P)
+            symbols.pop(number)
+            DPLL.search(self, clauses, symbols, model, P, value)
+        P = symbols[0]
+        rest = symbols[1:];
+        print (symbols)
+        return ((DPLL.search(self, clauses, rest, model, P, True)) 
+            or (DPLL.search(self, clauses, rest, model, P, False)))
+            
+    def check_false_clauses(self, clauses, model):
+        if not model:
+            # if there is nothing in the model just return some number so it can continue to find the assignment
+            return -1
         else:
-            [P, value] = DPLL.find_pure_symbol(self, symbols, clauses, model)
-            if P is not None:
-                number = DPLL.pop_symbol(self, symbols, P)
-                symbols.pop(number)
-                model[P] = value;
-                DPLL.search(self, clauses, symbols, model)
-            [P, value] = DPLL.find_unit_clause(self, symbols, clauses, model)
-            if P is not None:
-                number = DPLL.pop_symbol(self, symbols, P)
-                symbols.pop(number)
-                model[P] = value
-                DPLL.search(self, clauses, symbols, model)
-            print (symbols)
-            P = symbols[1]
-            rest = symbols[2:];
-            model[P] = True;
-            if (DPLL.search(self, clauses, rest, model)):
-                return True;
-            model[P] = False
-            if (DPLL.search(self, clauses, rest, model)):
-                return True;
+            for i in range(0, len(clauses)):
+                count = 0;                    
+                for k in clauses[i]:
+                    for j in model:
+                        if j == k:
+                            if clauses[i][k] != model[j]:
+                                count = count + 1;
+                if count == len(clauses[i]):
+                    # This means that there is at least one clauses that all of the literals are false
+                    return 0
+            
+            return -1                
+        
     
     def find_unit_clause(self, symbols, clauses, model):
         
@@ -86,17 +98,16 @@ class DPLL(object):
             return -1
         else:
             clauses_true = 0;
-            for j in model:
             # check for repeated clauses!!!!!
-                for i in range(0, len(clauses)):
-                    count = 0;                    
-                    for k in clauses[i]:
-                        
+            for i in range(0, len(clauses)):
+                count = 0;                    
+                for k in clauses[i]:
+                    for j in model:
                         if j == k:
                             if clauses[i][k] == model[j]:
                                 count = count + 1;
-                    if count > 0:
-                        clauses_true = clauses_true + 1;
+                if count > 0:
+                    clauses_true = clauses_true + 1;
             
             return clauses_true;
                         
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from GSAT import GSAT
 from WalkSAT import WalkSAT
 from DPLL import DPLL
 
-file = readFile('input_1.cnf')
+file = readFile('uf20-01.cnf')
 file = file.openFile()
 
 KB = file.readClauses(file)

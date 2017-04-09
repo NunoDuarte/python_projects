9c0056204746dea870154a15b3a7fba0dc853246
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-04-10 00:36:54 +0100

    add an image to my game

--- testpyGame.py
+++ testpyGame.py
@@ -2,8 +2,19 @@ import pygame
 
 pygame.init()
 
+display_width = 800
+display_height = 600
+
+#colors definition
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+
 #resolution
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 
 #name of the display
 pygame.display.set_caption('A bit Racey')
@@ -11,9 +22,18 @@ pygame.display.set_caption('A bit Racey')
 #imposes the time to everything in the game
 clock = pygame.time.Clock()
 
+
+carImg = pygame.image.load('racecar.png')
+
+#for displaying the car
+def car(x,y):
+    gameDisplay.blit(carImg, (x,y)) # x,y is one parameter (a tuple)
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
 #when we start we haven't crashed yet
 crashed = False
-
 while not crashed:
     
     # what key are they pressing?
@@ -22,10 +42,13 @@ while not crashed:
             # you've crashed
             crashed = True
             
-        print(event)
+    #the background color will now be white
+    gameDisplay.fill(white)
+    
+    #put the car in the game
+    car(x,y)
 
     pygame.display.update()
-    
     #frame per second #how fast things move
     clock.tick(60)
 

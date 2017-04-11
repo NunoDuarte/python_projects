5123c0cc1a7f365afcb4e0bd1b9b12e1f4eecb10
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-04-11 23:53:29 +0100

    Stopping our car from driving outside the borders of our image

--- testpyGame.py
+++ testpyGame.py
@@ -24,49 +24,59 @@ clock = pygame.time.Clock()
 
 
 carImg = pygame.image.load('racecar.png')
+car_width = 73 #we know the width of our image
 
 #for displaying the car
 def car(x,y):
     gameDisplay.blit(carImg, (x,y)) # x,y is one parameter (a tuple)
 
-x = (display_width * 0.45)
-y = (display_height * 0.8)
-
-x_change = 0
-
-#when we start we haven't crashed yet
-crashed = False
-while not crashed:
+def game_loop():
     
-    # what key are they pressing?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            # you've crashed
-            crashed = True
-            
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-                
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
-                
-    x += x_change
-                
-            
-            
-    #the background color will now be white
-    gameDisplay.fill(white)
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
     
-    #put the car in the game
-    car(x,y)
-
-    pygame.display.update()
-    #frame per second #how fast things move
-    clock.tick(60)
+    x_change = 0
+    
+    #when we start we haven't left the game yet (duh!!)
+    gameExit = False
+    while not gameExit:
+        
+        # what key are they pressing?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # you've left the game
+                gameExit = True
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+                    
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+                    
+        x += x_change
+                    
+                
+                
+        #the background color will now be white
+        gameDisplay.fill(white)
+        
+        #put the car in the game
+        car(x,y)
+        
+        if x > display_width - car_width or x < 0:
+            #you've crashed
+            gameExit = True
+        
+    
+        pygame.display.update()
+        #frame per second #how fast things move
+        clock.tick(60)
+        
+game_loop()
 
 pygame.quit()
 quit()

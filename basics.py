8ca8cf7ea1d2482d3a4ade0dfb010d641796be06
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-05-25 18:27:13 +0100

    learning about images and different colors; still very basic

--- basics.py
+++ basics.py
@@ -1,13 +1,15 @@
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
 import PIL
 from PIL import Image
 
-i = Image.open('images/dot.png')
+i = Image.open('images/numbers/y0.5.png')
 # image array -> convert to a np.array (3D array)
 iar = np.array(i)
 
-print (iar)
+plt.imshow(iar)
+print(iar)
+plt.show()
 
 # first group of numbers is the first row. and the first line is the first pixel (4 numbers to define the pixel)
 #(RGB + alpha) the alpha is for transparency

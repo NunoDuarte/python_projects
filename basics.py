79d7c4c4840821cca88028d5904e5e413a11c669
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-05-26 23:55:46 +0100

    how to plot images, subplots, etc; still very basic

--- basics.py
+++ basics.py
@@ -1,19 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import PIL
+import time
 from PIL import Image
 
-i = Image.open('images/numbers/y0.5.png')
-# image array -> convert to a np.array (3D array)
-iar = np.array(i)
+def threshold(imageArray):
+    balanceAr = []
+    newAr = imageArray
+    
+    for eachRow in imageArray:
+        for eachPix in eachRow:
+            print (eachPix) 
+            
+            time.sleep(5)
+            
+i1 = Image.open('images/numbers/0.1.png')
+iar1 = np.array(i1)       
 
-plt.imshow(iar)
-print(iar)
+i2 = Image.open('images/numbers/y0.4.png')
+iar2 = np.array(i2)  
+        
+i3 = Image.open('images/numbers/y0.5.png')
+iar3 = np.array(i3)          
+
+i4 = Image.open('images/sentdex.png')
+iar4 = np.array(i4)          
+
+fig = plt.figure()
+ax1 = plt.subplot2grid((8,6), (0,0), rowspan=4, colspan=3)
+ax2 = plt.subplot2grid((8,6), (4,0), rowspan=4, colspan=3)
+ax3 = plt.subplot2grid((8,6), (0,3), rowspan=4, colspan=3)
+ax4 = plt.subplot2grid((8,6), (4,3), rowspan=4, colspan=3)
+
+ax1.imshow(iar1)
+ax2.imshow(iar2)
+ax3.imshow(iar3)
+ax4.imshow(iar4)
+
 plt.show()
 
-# first group of numbers is the first row. and the first line is the first pixel (4 numbers to define the pixel)
-#(RGB + alpha) the alpha is for transparency
-# big alpha means that it is very opaque (small transparency)
 
-
-
+     
+            
+            

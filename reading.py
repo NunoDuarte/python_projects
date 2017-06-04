bf75935aa398147a9cc8f2efac8baefa01f01683
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-06-04 16:18:32 +0100

    show different images corresponding to different slices of the lungs

--- reading.py
+++ reading.py
@@ -2,6 +2,9 @@ import dicom # read dicom files
 import os # do directory operations
 import pandas as pd # nice for data analysis
 # needed to change the python version
+import matplotlib.pyplot as plt 
+import cv2
+import numpy as np
 
 data_dir = '/Volumes/LaCie02/NUNO/stage1/'
 patients = os.listdir(data_dir)
@@ -17,5 +20,21 @@ for patient in patients[:10]:
     print(len(slices), slices[0].pixel_array.shape)
     # every patient has a 3D image with x images of 512x512.
     # first patient has 195 images the 2nd 265, etc
+    
+    
+IMG_PX_SLICE = 150
+
+for patients in patients[:2]:
+    label = labels_df.get_value(patient, 'cancer')
+    path = data_dir + patient
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: int(x.ImagePositionPatient[2]))
+    
+    fig = plt.figure()
+    for num, each_slice in enumerate(slices[:12]):
+        y = fig.add_subplot(3,4, num+1)
+        new_image = cv2.resize(np.array(each_slice.pixel_array), (IMG_PX_SLICE, IMG_PX_SLICE))
+        y.imshow(new_image)
+    plt.show()
     
     

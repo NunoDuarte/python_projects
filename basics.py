5cb65621b7acb9fbf4e0aa82752d43b097952d62
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-06-06 23:54:04 +0100

    count the different numbers

--- basics.py
+++ basics.py
@@ -4,6 +4,10 @@ import PIL
 import time
 from PIL import Image
 
+def createExamples():
+    numberArrayExamples = open('numArEx.txt', 'a')
+    numbersWeHave = range(1,10)
+
 def threshold(imageArray):
     balanceAr = []
     newAr = imageArray
@@ -26,18 +30,20 @@ iar3 = np.array(i3)
 i4 = Image.open('images/sentdex.png')
 iar4 = np.array(i4)          
 
-fig = plt.figure()
-ax1 = plt.subplot2grid((8,6), (0,0), rowspan=4, colspan=3)
-ax2 = plt.subplot2grid((8,6), (4,0), rowspan=4, colspan=3)
-ax3 = plt.subplot2grid((8,6), (0,3), rowspan=4, colspan=3)
-ax4 = plt.subplot2grid((8,6), (4,3), rowspan=4, colspan=3)
+# fig = plt.figure()
+# ax1 = plt.subplot2grid((8,6), (0,0), rowspan=4, colspan=3)
+# ax2 = plt.subplot2grid((8,6), (4,0), rowspan=4, colspan=3)
+# ax3 = plt.subplot2grid((8,6), (0,3), rowspan=4, colspan=3)
+# ax4 = plt.subplot2grid((8,6), (4,3), rowspan=4, colspan=3)
+# 
+# ax1.imshow(iar1)
+# ax2.imshow(iar2)
+# ax3.imshow(iar3)
+# ax4.imshow(iar4)
+# 
+# plt.show()
 
-ax1.imshow(iar1)
-ax2.imshow(iar2)
-ax3.imshow(iar3)
-ax4.imshow(iar4)
 
-plt.show()
 
 
      

523a2f65d5699b7fdb9b4729e0a74ce1ef4b77fd
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-06-26 16:07:21 +0100

    Solved the problem of having different data everytime I opened the file; The problem was the streams[0], the [0] wasnt always the same one previously. You need to specify the name to all get the exact one you want. In our case is Gaze primitive Data

--- pupil_src/receiveString.py
+++ pupil_src/receiveString.py
@@ -4,7 +4,9 @@ from pylsl import StreamInlet, resolve_stream
 
 # first resolve a marker stream on the lab network
 print("looking for a marker stream...")
-streams = resolve_stream('type', 'Pupil Capture')
+# type = 'Pupil Capture'
+# name = 'Gaze Primitive Data' or 'Gaze Python Representation'
+streams = resolve_stream('name', 'Gaze Python Representation')
 print("connected")
 
 # create a new inlet to read from the stream
@@ -15,4 +17,4 @@ while True:
     # interested in it)
     sample, timestamp = inlet.pull_sample()
     print("got %s at time %s" % (sample[0], timestamp))
-    print("norm pos x %s and norm pos y %s" % (sample[3], sample[4]))
+    #print("norm pos x %s and norm pos y %s" % (sample[3], sample[4]))

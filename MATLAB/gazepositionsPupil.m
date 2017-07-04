2de881ac2684d8c9153d15120aefe78fac425edf
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-07-04 18:03:23 +0100

    adding the exponential and triangular moving average; Conclusion: they arent better than the simple moving average

--- MATLAB/gazepositionsPupil.m
+++ MATLAB/gazepositionsPupil.m
@@ -11,11 +11,21 @@ window_size = 12;
 mv_norm_x = tsmovavg(norm_pos_x,'s',window_size, 1);
 mv_norm_y = tsmovavg(norm_pos_y,'s',window_size, 1);
 
+% exponential moving average
+window_size = 12; 
+emv_norm_x = tsmovavg(norm_pos_x,'e',window_size, 1);
+emv_norm_y = tsmovavg(norm_pos_y,'e',window_size, 1);
+
+% Triangular moving average
+window_size = 12; 
+tmv_norm_x = tsmovavg(norm_pos_x,'t',window_size, 1);
+tmv_norm_y = tsmovavg(norm_pos_y,'t',window_size, 1);
 
 timestamp(end,1)
 
 i = 1;
 n = timestamp(i,1);
+
 
 while(n < timestamp(end))
 
@@ -24,20 +34,39 @@ while(n < timestamp(end))
       set(gcf,'color','white');
       drawnow;
       
-      subplot(1,2,1)
+      subplot(1,4,1)
       plot(norm_pos_x(i,1), norm_pos_y(i,1),'-.dk','linewidth',1.8)
       axis([-1 1 -1 1])
       title('Pupil gaze in Image Plane');
       xlabel('norm pos x');
       ylabel('norm pos y');
       
-      subplot(1,2,2)
+      subplot(1,4,2)
       plot(mv_norm_x(i,1), mv_norm_y(i,1),'-.dk','linewidth',1.8)
       axis([-1 1 -1 1])
+      
       grid off;
       title('Simple Moving Average');
       xlabel('mv norm pos x');
       ylabel('mv norm pos y');
+      
+      subplot(1,4,3)
+      plot(emv_norm_x(i,1), emv_norm_y(i,1),'-.dk','linewidth',1.8)
+      axis([-1 1 -1 1])
+      
+      grid off;
+      title('Exponential Moving Average');
+      xlabel('exp mv norm pos x');
+      ylabel('exp mv norm pos y');      
+      
+      subplot(1,4,4)
+      plot(tmv_norm_x(i,1), tmv_norm_y(i,1),'-.dk','linewidth',1.8)
+      axis([-1 1 -1 1])
+      
+      grid off;
+      title('Triangular Moving Average');
+      xlabel('tri mv norm pos x');
+      ylabel('tri mv norm pos y');        
       
       n = timestamp(i,1);
       %pause(0.5);

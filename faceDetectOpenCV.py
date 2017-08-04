24c45ffbfea58e6cdb4fcc1a2a926e2edd392f6d
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-08-05 00:29:17 +0100

    corrected the code with the appropriate libraries

---
+++ faceDetectOpenCV.py
@@ -0,0 +1,15 @@
+import dlib
+import collections
+
+
+# define a dictionary that maps the indexes of the facial
+# landmarks to specific face regions
+FACIAL_LANDMARKS_IDXS = collections.OrderedDict([
+    ("mouth", (48, 68)),
+    ("right_eyebrow", (17, 22)),
+    ("left_eyebrow", (22, 27)),
+    ("right_eye", (36, 42)),
+    ("left_eye", (42, 48)),
+    ("nose", (27, 35)),
+    ("jaw", (0, 17))
+])

62e044ce4bee3cac282b3bbc274922be3a4ab22c
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-08-05 23:55:30 +0100

    add the drawmarks to our input image

--- faceDetectOpenCV.py
+++ faceDetectOpenCV.py
@@ -13,3 +13,19 @@ FACIAL_LANDMARKS_IDXS = collections.OrderedDict([
     ("nose", (27, 35)),
     ("jaw", (0, 17))
 ])
+
+def visualize_facial_landmarks(image, shape, colors=None, alpha=0.75):
+    # create two copies of the input image -- one for the
+    # overlay and one for the final output image
+    overlay = image.copy()
+    output = image.copy()
+ 
+    # if the colors list is None, initialize it with a unique
+    # color for each facial landmark region
+    if colors is None:
+        colors = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
+            (168, 100, 168), (158, 163, 32),
+            (163, 38, 32), (180, 42, 220)]
+        
+        
+        

a032d6facdd4388de3744651f29db4a54722e10a
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-08-06 23:57:17 +0100

    draw the jaw onto the image

--- faceDetectOpenCV.py
+++ faceDetectOpenCV.py
@@ -1,5 +1,6 @@
 import dlib
 import collections
+import cv2
 
 
 # define a dictionary that maps the indexes of the facial
@@ -26,6 +27,27 @@ def visualize_facial_landmarks(image, shape, colors=None, alpha=0.75):
         colors = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
             (168, 100, 168), (158, 163, 32),
             (163, 38, 32), (180, 42, 220)]
-        
+   
+    # loop over the facial landmark regions individually
+    for (i, name) in enumerate(FACIAL_LANDMARKS_IDXS.keys()):
+        # grab the (x, y)-coordinates associated with the
+        # face landmark
+        (j, k) = FACIAL_LANDMARKS_IDXS[name]
+        pts = shape[j:k]
+ 
+        # check if are supposed to draw the jawline
+        if name == "jaw":
+            # since the jawline is a non-enclosed facial region,
+            # just draw lines between the (x, y)-coordinates
+            for l in range(1, len(pts)):
+                ptA = tuple(pts[l - 1])
+                ptB = tuple(pts[l])
+                cv2.line(overlay, ptA, ptB, colors[i], 2)
+ 
+        # otherwise, compute the convex hull of the facial
+        # landmark coordinates points and display it
+        else:
+            hull = cv2.convexHull(pts)
+            cv2.drawContours(overlay, [hull], -1, colors[i], -1)     
         
         

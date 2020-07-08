10c5a7004a6bc3ecf1dc762574448a3ba3833516
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-07-08 23:16:09 +0200

    learning how to code in keras to build NN

---
+++ keras/simpleNN.py
@@ -0,0 +1,10 @@
+from keras import models
+from keras import layers
+
+model = models.Sequential()
+
+# first layer (output, input)
+model.add(layers.Dense(32, input_shape=(784,)))
+# second layer (only output)  - it takes the size of the input to be the same as the output of the 
+# previous layer
+model.add(layers.Dense(32))

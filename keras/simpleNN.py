4ba154cff323f991140566fc66c31ab01c1900a1
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-07-08 23:27:01 +0200

    read another section - continue on to the next one

--- keras/simpleNN.py
+++ keras/simpleNN.py
@@ -8,3 +8,5 @@ model.add(layers.Dense(32, input_shape=(784,)))
 # second layer (only output)  - it takes the size of the input to be the same as the output of the 
 # previous layer
 model.add(layers.Dense(32))
+
+# stop at section 3.4

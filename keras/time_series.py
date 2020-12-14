121d86f1083d443f86a8c85e8e95e0706951071c
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-12-14 14:38:25 +0000

    normalize data to have all the same size

--- keras/time_series.py
+++ keras/time_series.py
@@ -26,10 +26,42 @@ path = "~/datasets/MovementAAL/dataset/MovementAAL_RSS_"
 sequences = list()
 for i in range(1,315):
     file_path = path + str(i) + '.csv'
-    print(file_path)
+    #print(file_path)
     df = pd.read_csv(file_path, header=0)
     values = df.values
     sequences.append(values)
 
+targets = pd.read_csv("~/datasets/MovementAAL/dataset/MovementAAL_target.csv")
+targets = targets.values[:,1]
+
+# print the first sequence of the dataset (4 sensors over the duration of the action)
+#print(sequences[0])
+
+groups = pd.read_csv("~/datasets/MovementAAL/groups/MovementAAL_DatasetGroup.csv", header=0)
+groups = groups.values[:,1]
+
+# normalize data
+len_sequences = []
+for one_seq in sequences:
+    len_sequences.append(len(one_seq))
+print(pd.Series(len_sequences).describe())
+
+#Padding the sequence with the values in last row to max length
+to_pad = 129
+new_seq = []
+for one_seq in sequences:
+    len_one_seq = len(one_seq)
+    last_val = one_seq[-1]
+    n = to_pad - len_one_seq
+   
+    to_concat = np.repeat(one_seq[-1], n).reshape(4, n).transpose()
+    new_one_seq = np.concatenate([one_seq, to_concat])
+    new_seq.append(new_one_seq)
+final_seq = np.stack(new_seq)
+
+#truncate the sequence to length 60
+from keras.preprocessing import sequence
+seq_len = 60
+final_seq=sequence.pad_sequences(final_seq, maxlen=seq_len, padding='post', dtype='float', truncating='post')
 
 

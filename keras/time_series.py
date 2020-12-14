3e690fb825eca2cf48751c25ffc1f1c4f9f56b7d
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-12-14 11:52:05 +0000

    add keras and tensorflow to the time series classification

--- keras/time_series.py
+++ keras/time_series.py
@@ -3,3 +3,15 @@ import numpy as np
 # this is for jupyter-notebook %matplotlib inline
 import matplotlib.pyplot as plt
 from os import listdir
+
+from keras.preprocessing import sequence
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+from keras.optimizers import Adam
+from keras.models import load_model
+from keras.callbacks import ModelCheckpoint
+
+

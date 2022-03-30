b8d5fd2d9fb505df68878f7aaf265bb47f72a6f2
Nuno Duarte <nunoduarte.cc.fduarte@gmail.com>
2022-03-30 12:02:10 +0100

    training, testing, plotting results

--- Keras/imdb_classification.py
+++ Keras/imdb_classification.py
@@ -54,11 +54,12 @@ partial_x_train = x_train[10000:]
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
 
+n_epochs = 40
 
 history = model.fit(partial_x_train,
                     partial_y_train,
-                    epochs=20,
-                    batch_size=512,
+                    epochs=n_epochs,
+                    batch_size=1024,
                     validation_data=(x_val, y_val))
 
 # plot the results
@@ -66,7 +67,7 @@ history_dict = history.history
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
 
-epochs = range(1, 20  + 1)
+epochs = range(1, n_epochs  + 1)
 
 plt.plot(epochs, loss_values, 'bo', label='Training loss')
 plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
@@ -75,4 +76,17 @@ plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
 plt.show()
+
+plt.clf() # clear figure
+
+acc_values = history_dict['accuracy'] 
+val_acc_values = history_dict['val_accuracy']
+plt.plot(epochs, acc_values, 'bo', label='Training acc')
+plt.plot(epochs, val_acc_values, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
 

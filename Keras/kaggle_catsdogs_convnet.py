127a9448fedf584570b5a1d13d6252633b73438d
Nuno Duarte <nunoduarte.cc.fduarte@gmail.com>
2022-04-01 14:22:10 +0100

    learn about data augmentation using keras

--- Keras/kaggle_catsdogs_convnet.py
+++ Keras/kaggle_catsdogs_convnet.py
@@ -1,6 +1,7 @@
 from keras import layers
 from keras import models
 from keras import optimizers
+from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator # for image processing
 import matplotlib.pyplot as plt
 
@@ -49,6 +50,39 @@ validation_generator = test_datagen.flow_from_directory(
         target_size=(150, 150),
         batch_size=20,
         class_mode='binary')
+
+# ---------- // ----------
+## UPDATE - Data Augmentation
+import os 
+
+train_cats_dir = '/home/nuno/datasets/dogs-vs-cats/cats_and_dogs_small/train/cats'
+
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+fnames = [os.path.join(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
+img_path = fnames[3]
+img = image.load_img(img_path, target_size=(150, 150))
+
+x = image.img_to_array(img)
+x = x.reshape((1,) + x.shape)
+
+i=0
+for batch in datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i += 1
+    if i % 4 == 0:
+        break
+plt.show()
+
+input()
 
 # train model
 history = model.fit(

add50a56a6b8bf40e0e605a9799e35f10d9a590d
Nuno Duarte <nunoduarte.cc.fduarte@gmail.com>
2022-04-01 17:30:04 +0100

    change code to make it work as intended

---
+++ Keras/kaggle_catsdogs_convnetVGG16v2.py
@@ -0,0 +1,93 @@
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator # for image processing
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.applications import VGG16
+
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
+
+# Freezing - prevent the weights of VGG16 to be updated
+# with this commented it achieves the accuracy that the book presents;
+# with this not commneted, it only gets to 89%
+#conv_base.trainable = False
+
+# get dataset
+dataset_dir = '/home/nuno/datasets/dogs-vs-cats/cats_and_dogs_small'
+train_dir = dataset_dir + '/train'
+validation_dir = dataset_dir + '/validation'
+test_dir = dataset_dir + '/test'
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+## process data 
+# train
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+# validation
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+# build CNN
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+# choose compiler, loss function, and accuracy as metric
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+# train model
+history = model.fit(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=30,
+      validation_data=validation_generator,
+      validation_steps=50)
+
+model.save('cats_and_dogs_small_4.h5')
+
+# plot results
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+

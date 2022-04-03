ae238224591cb84fbfd432fe6053c390e0f8d394
Nuno Duarte <nunoduarte.cc.fduarte@gmail.com>
2022-04-03 21:04:25 +0100

    working on plotting all the output for the layers of the CNN

--- Keras/kaggle_catsdogs_CNN/visualize_convnet.py
+++ Keras/kaggle_catsdogs_CNN/visualize_convnet.py
@@ -1,8 +1,8 @@
 from keras.models import load_model
 from keras.preprocessing import image
+from keras import models
 import numpy as np
 import matplotlib.pyplot as plt
-
 
 model = load_model('models/cats_and_dogs_small_2.h5')
 #model.summary()
@@ -15,4 +15,53 @@ img_tensor = np.expand_dims(img_tensor, axis=0)
 
 plt.imshow(img_tensor[0])
 plt.show()
+
+layer_outputs = [layer.output for layer in model.layers[:8]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+
+activations = activation_model.predict(img_tensor)
+first_layer_activation = activations[0]
+print(first_layer_activation.shape)
+
+#plt.matshow(first_layer_activation[0, :, :, 7], cmap='viridis')
+#plt.show()
+
+layer_names = []
+for layer in model.layers[:8]:
+    layer_names.append(layer.name)
+
+images_per_row = 16
+
+for layer_name, layer_activation in zip(layer_names, activations):
+
+    n_features = layer_activation.shape[-1]
+    
+    size = layer_activation.shape[1]
+
+    n_cols = n_features // images_per_row
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = layer_activation[0, :, :, col* images_per_row + row]
+
+            channel_image -= channel_image.mean()
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            display_grid[col * size : (col + 1) * size, 
+                    row * size : (row + 1) * size] = channel_image
+
+    scale = 1. / size
+
+    plt.figure(figsize=(scale * display_grid.shape[1],
+                    scale * display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+
+
+
 

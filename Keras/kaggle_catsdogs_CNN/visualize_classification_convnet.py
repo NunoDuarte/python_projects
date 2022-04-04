752f8444d6b7fb0c6a90af8e6de0da65cd15fbf9
Nuno Duarte <nunoduarte.cc.fduarte@gmail.com>
2022-04-04 16:10:24 +0100

    superimpose heatmap onto image

--- Keras/kaggle_catsdogs_CNN/visualize_classification_convnet.py
+++ Keras/kaggle_catsdogs_CNN/visualize_classification_convnet.py
@@ -6,6 +6,7 @@ from keras import backend as K  # perform gradient descent
 from keras.applications.vgg16 import preprocess_input, decode_predictions
 import tensorflow as tf
 import numpy as np
+import scipy.ndimage
 import matplotlib.pyplot as plt
 
 model = VGG16(weights='imagenet')
@@ -34,9 +35,25 @@ with tf.GradientTape() as gtape:
     grads = gtape.gradient(loss, conv_output)
     pooled_grads = K.mean(grads, axis=(0, 1, 2))
 
+# show Heatmap of CAM
 heatmap = np.mean(tf.multiply(pooled_grads, conv_output), axis=-1)
 heatmap = np.maximum(heatmap,0)
 heatmap /= np.max(heatmap)
 plt.matshow(heatmap[0], cmap='jet')
+#plt.show()
+
+## superimpose on image
+# scipy interpolation to rescale the heatmap to the size of the img
+# 224x224 - 14x14 = Resample by a factor of 16
+heatmap = scipy.ndimage.zoom(heatmap[0], 16, order=1)
+heatmap = np.uint8(255 * heatmap) # convert to RGB
+
+plt.figure()
+plt.subplot(1,2,1)
+plt.imshow(img, 'gray', interpolation='none')
+plt.subplot(1,2,2)
+plt.imshow(heatmap,'jet', interpolation='none', alpha=0.7)
+plt.imshow(img, interpolation='none',alpha=0.5)
 plt.show()
+
 
